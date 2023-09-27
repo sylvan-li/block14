5778b59ef5795258472cece59d8b8867bd9e4079
sylvan-li <76680992+sylvan-li@users.noreply.github.com>
2023-09-27 16:28:22 -0700

    Update block14exercise.js
    added psuedocode

--- block14exercise.js
+++ block14exercise.js
@@ -1,4 +1,6 @@
 //only odds 
+//create a var for numbers input 
+// for loop, if numbers give a remainder, they are odd - put in different list - print that list out 
 var numbers=[2,4,6,8,11,20,15,22];
 
 const odds =[];
@@ -11,6 +13,8 @@ console.log(odds);
 // if (num%2 ==0)
 
 //vowels vs consonant
+//create a variable for both vowelcounts and const, establish a array for vowels,
+//put all other not counted in array into const count. 
 
 var vowels=["a", "e", "i", "o", "u"];
 const inputArr = "hello"
@@ -29,6 +33,8 @@ for(const char of inputArr){
 console.log(inputArr, "has", vowelcount, "vowels and", constcount, "constanents");
 
 //ReverseArray
+//use num reverse to reverse the numbers of the array 
+
 var numms=[1,2,3];
 
 numms.reverse();
@@ -36,6 +42,10 @@ numms.reverse();
 console.log(numms);
 
 //fizzbuzz//
+// create an array with range of less than 101 to get the full range of 100, 
+//create conditions for diving by 3 with an output of fizz, 
+// create conditions for dividing by 5 with an output of buzz 
+// else put fizzbuzz for those that dont meet the first two conditons 
 for (var i = 1; i<101;i++){
     if (i% 15 ==0) console.log("FizzBuzz");
     else if (i%3 == 0) console.log("Fizz");
